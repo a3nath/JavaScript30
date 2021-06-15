29122b12575ee21807ac33d9555055b1b38eacd3
a3nath <amarn.sks@gmail.com>
2021-06-14 22:35:17 -0400

    key up

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,12 +1,16 @@
 document.addEventListener('keydown', function (event) {
     let selKey = null;
     let audio = null;
-    switch(event.key){
+    const downKey = event.key
+    switch(downKey){
         case("a"):
             selKey = document.querySelector("div[data-key='65']")
             selKey.classList.add("playing")
-            audio = document.querySelector("audio[data-key='65']")
-            audio.play()
+            console.log(selKey.innerHTML)
+            console.log(selKey.classList.value)
+            // audio = document.querySelector("audio[data-key='65']")
+            // audio.play()
+            // console.log(audio.innerHTML)
         case("s"):
             selKey = document.querySelector("div[data-key='83']")
             selKey.classList.add("playing")
@@ -49,4 +53,41 @@ document.addEventListener('keydown', function (event) {
             audio.play()
     }
   });
+
+//   document.addEventListener('keyup', function (event) {
+//     let upSelKey = null;
+//     const upKey = event.key
+//     switch(upKey){
+//         case("a"):
+//             upSelKey = document.querySelector("div[data-key='65']")
+//             upSelKey.classList.remove("playing")
+//         case("s"):
+//             upSelKey = document.querySelector("div[data-key='83']")
+//             upSelKey.classList.remove("playing")
+//         case("d"):
+//             upSelKey = document.querySelector("div[data-key='68']")
+//             upSelKey.classList.remove("playing")  
+//         case("f"):
+//             upSelKey = document.querySelector("div[data-key='70']")
+//             upSelKey.classList.remove("playing")
+//         case("g"):
+//             upSelKey = document.querySelector("div[data-key='71']")
+//             upSelKey.classList.remove("playing")
+//         case("h"):
+//             upSelKey = document.querySelector("div[data-key='72']")
+//             upSelKey.classList.remove("playing")
+//         case("j"):
+//             upSelKey = document.querySelector("div[data-key='74']")
+//             upSelKey.classList.remove("playing")
+//         case("k"):
+//             upSelKey = document.querySelector("div[data-key='75']")
+//             upSelKey.classList.remove("playing")
+//         case("l"):
+//             upSelKey = document.querySelector("div[data-key='76']")
+//             upSelKey.classList.remove("playing")
+                  
+//     }
+//   });
+
+
 
